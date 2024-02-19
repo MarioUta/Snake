a631b04c1fe0a4fd0426041aa1ea32a398c793f4
Mario Uță <115089577+MarioUta@users.noreply.github.com>
2024-02-19 02:22:14 +0200

    Update Snake.cpp

--- Snake.cpp
+++ Snake.cpp
@@ -11,27 +11,6 @@ Snake::Snake() {
     head = new Node;
     head->x = 9;
     head->y = 13;
-    /*
-    Node *second = new Node, *third = new Node, *forth = new Node, *fifth = new Node;
-    head->prev = nullptr;
-    head->next = second;
-    second->prev = head;
-    second->next = third;
-    third->prev = second;
-    third->next = forth;
-    forth->prev = third;
-    forth->next = fifth;
-    fifth->prev = forth;
-    fifth->next = nullptr;
-    second->x = 12;
-    second->y = 13;
-    third->x = 11;
-    third->y = 13;
-    forth->x = 10;
-    forth->y = 13;
-    fifth->x = 9;
-    fifth->y = 13;
-     */
 }
 
 void Snake::move(int direction) {
